d8bd6ede8862079130be4b71dd999b34bcb404c7
siyin yang <yangsiyin.bao@gmail.com>
2021-07-05 14:01:47 +1000

    Update ProductProfile.cs

--- ProductApi/Helpers/Mapper/ProductProfile.cs
+++ ProductApi/Helpers/Mapper/ProductProfile.cs
@@ -9,9 +9,11 @@ namespace ProductApi.Helpers.Mapper
         public ProductProfile()
         {
             CreateMap<Product, ProductDto>()
-                .ForMember(target => target.ProductOptions,
-                    map => map.MapFrom(src => src.ProductOptions))
-                .ReverseMap();
+                .ForMember(target => target.ProductOptions, map =>
+                    map.MapFrom(src => src.ProductOptions));
+            CreateMap<ProductDto, Product>()
+                .ForMember(target => target.ProductOptions, map =>
+                    map.MapFrom(src => src.ProductOptions));
             CreateMap<ProductOption, ProductOptionDto>().ReverseMap();
         }
     }
